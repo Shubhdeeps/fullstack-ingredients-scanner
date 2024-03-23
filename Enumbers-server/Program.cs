7ec71a5814215f6ff1f338cae35bf3ec65ff1879
Shubhdeeps <shubhdeep86993@gmail.com>
2024-03-23 13:55:13 +0200

    readme, reactjs init

--- Enumbers-server/Program.cs
+++ Enumbers-server/Program.cs
@@ -3,9 +3,6 @@ using Enumbers_server.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-//set app host and port
-app.Urls.Add("http://localhost:9090");
-
 app.MapGamesEndpoints();
 app.MapOCREndpoints();
 app.MapGet("/", () => Results.Text("Hello, world!"));
