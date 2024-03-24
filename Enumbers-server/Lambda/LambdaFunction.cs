c623f71c5603587cd45ec840b7ff7fd9e9767152
Shubhdeeps <shubhdeep86993@gmail.com>
2024-03-24 12:47:35 +0200

    save

---
+++ Enumbers-server/Lambda/LambdaFunction.cs
@@ -0,0 +1,11 @@
+
+public class LambdaFunction : Amazon.Lambda.AspNetCoreServer.APIGatewayProxyFunction
+{
+    protected override void Init(IWebHostBuilder builder)
+    {
+        builder
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseLambdaServer()
+            .UseApiGateway();
+    }
+}
